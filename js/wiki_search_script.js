fc0170ac7cbec9a362ef4b272b7f5537935d2ace
rustam <rustam.kamberov@gmail.com>
2017-08-08 10:57:02 +0200

    implemented search and autocomplete functionality

--- js/wiki_search_script.js
+++ js/wiki_search_script.js
@@ -1,13 +1,37 @@
 $(document).ready(function() {
 
+    // enableAutocomplete();
+    $("#search-input").autocomplete({
+        source: function(req, res) {
+            $.ajax({
+                url: "http://en.wikipedia.org/w/api.php",
+                dataType: "jsonp",
+                data: {
+                    'action': "opensearch",
+                    'format': "json",
+                    'search': req.term
+                },
+                success: function(data){
+                    res(data[1]);
+                    console.log(req.term);
+                }
+            });
+        }
+    });
+});
+
+function search(){
+    var input = $("#search-input").val();
+    //var input = document.getElementById("search-input").value;
+    console.log("input is: ", input);
     var wikiRequestTimeout = setTimeout(function(){
             //$wikiElem.text("Failed getting Wiki resources");
         }, 8000);
     var cityTyped = 'butterflies';
 
 
-    var wikiUrl ='http://en.wikipedia.org/w/api.php?action=query&generator=random&grnnamespace=0&prop=extracts&exchars=500&format=json';
-    //var wikiUrl ='http://en.wikipedia.org/w/api.php?action=opensearch&search=' + cityTyped + '&format=json&callback=wikiCallback';
+    //var wikiUrl ='http://en.wikipedia.org/w/api.php?action=query&generator=random&grnnamespace=0&prop=extracts&exchars=500&format=json';
+    var wikiUrl ='http://en.wikipedia.org/w/api.php?action=opensearch&search=' + input + '&format=json';
         console.log(wikiUrl);
 
     $.ajax({
@@ -30,5 +54,7 @@ $(document).ready(function() {
             }
         }
     });
+    return false;
+}
 
-});
+$('#form-container').submit(search);
